4b887bbcf29ec666bb081897011253236b32a82f
Hanieh <hmfarsibaf@unm.edu>
2018-01-05 17:02:11 -0700

    update documentation for MIC_IRSyringPump.m

--- MIC_IRSyringPump.m
+++ MIC_IRSyringPump.m
@@ -1,13 +1,16 @@
 classdef MIC_IRSyringPump < MIC_ThorlabsIR
-    %MIC_IRSyringPump creates an object to start Syringe Pump in the middle 
-    %of sequence for IRcamera from Thorlabs company. 
+    % MIC_IRSyringPump Matlab Instrument Class for using Syringe Pump at
+    % the same time of taking data with IRCamera
     %
-    %   REQUIREMENTS
+    %This class is only for SPT microscopy in Lidke's Lab 
+    %
+    % REQUIREMENTS: 
+    %   MIC_Abstract.m
     %   MIC_ThorlabsIR
     %   MIC_SyringePump
+    %   MATLAB software version R2016b or later
     %
-    % CITATION:
-    % Hanieh Mazloom-Farsibaf   Aug 2017 (Keith A. Lidke's lab)
+    % CITATION: Hanieh Mazloom-Farsibaf  Lidkelab, 2017.
         
     properties
         SP          % an obj for MIC_SyringePump to control Syringe pump
@@ -20,21 +23,21 @@ classdef MIC_IRSyringPump < MIC_ThorlabsIR
         end
         
         function start_sequence(obj)
+            %start_sequence for IR Camera and starting Syringe Pump in the
+            %middle of taking image by IR Camera
             obj.AcquisitionType='sequence';
             obj.setup_acquisition;
             if isempty(obj.ROI)
                 obj.ROI=[1,obj.XPixels,1,obj.YPixels];
             end
+            %init empty array
             obj.Data=zeros(obj.ROI(2),obj.ROI(4),obj.SequenceLength);
             obj.AbortNow=0;
-            %init empty array
             for ii=1:obj.SequenceLength
                 ii;
                 if ii==obj.SPwaitTime
                     obj.SP.run
-                    
                 end
-                
                 s32Wait=uc480.Defines.DeviceParameter.Wait;
                 obj.Cam.Acquisition.Freeze(s32Wait);
                 out=obj.getlastimage;
@@ -44,7 +47,7 @@ classdef MIC_IRSyringPump < MIC_ThorlabsIR
                     obj.AbortNow=0;
                     break
                 end
-                
+                %save data
                 if obj.KeepData
                     obj.Data(:,:,ii)=out;
                 end
