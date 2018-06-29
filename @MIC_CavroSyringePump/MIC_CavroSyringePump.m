cda791650e819afc7d6a7647cca7b94402e5ffd3
dschodt <schodtd@unm.edu>
2018-06-29 11:30:36 -0600

    Update class description for MIC_CavroSyringePump.m

--- @MIC_CavroSyringePump/MIC_CavroSyringePump.m
+++ @MIC_CavroSyringePump/MIC_CavroSyringePump.m
@@ -1,8 +1,22 @@
 classdef MIC_CavroSyringePump < MIC_Abstract
     % MIC class for control of the Cavro syringe pump PN 20740556 -D. 
     %
-    % MATLAB 2014b or later required.
-    % MATLAB R2017a or later recommended. 
+    % This class is used to control a Cavro syringe pump via USB.  This
+    % class may work for a wide range of Cavro brand syringe pumps, however
+    % it has only been tested for pump PN 20740556 -D .  It can perform any
+    % syringe pump operation described in the Cavro XP3000 operators manual
+    % (e.g. in Appendix G - Command Quick Reference). 
+    %
+    % Example: Pump = MIC_CavroSyringePump();
+    % Functions: delete, exportState, updateGui, gui, connectSyringePump,
+    %            readAnswerBlock, executeCommand, reportCommand, 
+    %            querySyringePump, cleanAnswerBlock, decodeStatusByte, 
+    %            unitTest
+    %
+    % REQUIREMENTS:
+    %   MATLAB 2014b or later required.
+    %   MATLAB R2017a or later recommended. 
+    %   MIC_Abstract.m
     %
     % CITATION: David Schodt, Lidke Lab, 2018
     
