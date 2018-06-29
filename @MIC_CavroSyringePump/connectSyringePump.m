c0d85a80a09f09851d45b5e05a73b5380ff0dc50
dschodt <schodtd@unm.edu>
2018-06-29 13:47:41 -0600

    Modify MATLAB versioning in connectSyringePump of MIC_CavroSyringePump

--- @MIC_CavroSyringePump/connectSyringePump.m
+++ @MIC_CavroSyringePump/connectSyringePump.m
@@ -8,10 +8,6 @@ function connectSyringePump(obj)
 % CITATION: David Schodt, Lidke Lab, 2018
 
 
-% Find the release version of the calling distribution of MATLAB.
-MatlabRelease = version('-release');
-MatlabYear = str2double(MatlabRelease(1:4)); 
-
 % Ensure that no COM object exists for the user specified port.
 SerialObjectList = instrfindall; 
 if ~isempty(SerialObjectList) 
@@ -32,12 +28,12 @@ if ~isempty(SerialObjectList)
 end
 
 % Create a serial object for the device and open the connection.
-if MatlabYear >= 2017
+if str2double(obj.MatlabRelease(1:4)) >= 2017
     ConnectedDevices = seriallist; % array of connected serial devices
 else
     % This version of Matlab does not have the seriallist function,
     % workaround this problem in the code below.
-    ConnectedDevices = ''; 
+    ConnectedDevices = '';
 end
 ValidPortGiven = 0;
 tic % begin a timer
