0b0450c0359d1e34ab04678f607b96ff90b8c4f8
dschodt <schodtd@unm.edu>
2018-07-02 10:58:03 -0600

    Move status waiting procedure to separate method, MIC_CavroSyringePump

--- @MIC_CavroSyringePump/executeCommand.m
+++ @MIC_CavroSyringePump/executeCommand.m
@@ -26,20 +26,6 @@ end
 
 % Send the command to the syringe pump. 
 fprintf(obj.SyringePump, Command);
-
-% Do not exit this method until the syringe pump is ready to accept new
-% commands (presumably because the current command was executed). 
-% NOTE: Once a GUI is implemented, this functionality will likely be 
-% shifted to a superclass calling upon this one. 
-QueryNumber = 1; 
-while (obj.StatusByte < 96) || (QueryNumber == 1)
-    % If obj.StatusByte < 96, the syringe pump is busy and we should query
-    % the device (which internally updates obj.StatusByte).  The OR
-    % condition ensures that at least one query is performed before
-    % exiting this method call. 
-    obj.querySyringePump; 
-    QueryNumber = QueryNumber + 1; 
-end
     
     
 end
