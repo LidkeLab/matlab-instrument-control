e2787d4b5279396a4644a31a7ab799f41ce08c38
dschodt <schodtd@unm.edu>
2018-07-03 09:46:36 -0600

    Improve error handling in MIC_CavroSyringePump/executeCommand

--- @MIC_CavroSyringePump/executeCommand.m
+++ @MIC_CavroSyringePump/executeCommand.m
@@ -25,20 +25,13 @@ else
 end
 
 % Send the command to the syringe pump. 
-fprintf(obj.SyringePump, Command);
-
-% Do not exit this method until the syringe pump is ready to accept new
-% commands (presumably because the current command was executed). 
-% NOTE: Once a GUI is implemented, this functionality will likely be 
-% shifted to a superclass calling upon this one. 
-QueryNumber = 1; 
-while (obj.StatusByte < 96) || (QueryNumber == 1)
-    % If obj.StatusByte < 96, the syringe pump is busy and we should query
-    % the device (which internally updates obj.StatusByte).  The OR
-    % condition ensures that at least one query is performed before
-    % exiting this method call. 
-    obj.querySyringePump; 
-    QueryNumber = QueryNumber + 1; 
+if ~isempty(obj.SyringePump)
+    % A syringe pump serial object exists, send the command.
+    fprintf(obj.SyringePump, Command);
+else
+    % No syringe pump serial object exists, tell the user they need to
+    % establish a connection.
+    error('Syringe pump not connected.')
 end
     
     
