eaabe43493aa98efe003f7e5ead6fd96776e7b3d
dschodt <schodtd@unm.edu>
2018-09-21 12:44:19 -0600

    Remove hard-coded addpath in MIC_StepperMotor.m

--- @MIC_StepperMotor/MIC_StepperMotor.m
+++ @MIC_StepperMotor/MIC_StepperMotor.m
@@ -40,7 +40,7 @@ classdef MIC_StepperMotor < MIC_Abstract
         function obj = MIC_StepperMotor(SerialNum)
             %constructor start the communications with all three motors and
             %also sets some of the class properties.
-            addpath('C:\Users\lidkelab\Documents\MATLAB\matlab-instrument-control\mex64');
+%             addpath('C:\Users\lidkelab\Documents\MATLAB\matlab-instrument-control\mex64');
             obj=obj@MIC_Abstract(~nargout);
             obj.SerialN = SerialNum;
             Kinesis_SBC_Open(obj.SerialN);
