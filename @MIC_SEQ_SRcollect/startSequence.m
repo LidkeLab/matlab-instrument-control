b9180b983740eb50ff920ab72604f28862967757
dschodt <schodtd@unm.edu>
2018-10-01 14:05:48 -0600

    Modify photobleaching behavior in MIC_SEQ_SRcollect.startSequence()

--- @MIC_SEQ_SRcollect/startSequence.m
+++ @MIC_SEQ_SRcollect/startSequence.m
@@ -1,52 +1,63 @@
- function startSequence(obj,RefStruct,LabelID)
-    %Collects and saves an SR data Set
+ function startSequence(obj, RefStruct, LabelID)
+    %Collects and saves a SR data Set
 
-    %Setup file saving
-    if ~obj.IsBleach
-        [~,~]=mkdir(obj.TopDir);
-        [~,~]=mkdir(fullfile(obj.TopDir,obj.CoverslipName));
-        DN=fullfile(obj.TopDir,obj.CoverslipName,sprintf('Cell_%2.2d',RefStruct.CellIdx),sprintf('Label_%2.2d',LabelID));
-        [~,~]=mkdir(DN);
-        TimeNow=clock;
-        DateString=[num2str(TimeNow(1)) '-' num2str(TimeNow(2))  '-' num2str(TimeNow(3)) '-' num2str(TimeNow(4)) '-' num2str(TimeNow(5)) '-' num2str(round(TimeNow(6)))];
-        FN=sprintf('Data_%s.h5',DateString);
-        FileName=fullfile(DN,FN);
+    % Setup directories/filenames as needed.
+    DirectoryName = fullfile(obj.TopDir, obj.CoverslipName, ...
+        sprintf('Cell_%2.2d', RefStruct.CellIdx), ...
+        sprintf('Label_%2.2d', LabelID));
+    mkdir(DirectoryName);
+    CurrentTime = clock;
+    DateString = [num2str(CurrentTime(1)), '-', ...
+        num2str(CurrentTime(2)),  '-', num2str(CurrentTime(3)), '-', ...
+        num2str(CurrentTime(4)), '-', num2str(CurrentTime(5)), '-', ...
+        num2str(round(CurrentTime(6)))];
+    if obj.IsBleach
+        % Indicate that this is a photobleaching sequence if necessary.
+        FileName = sprintf('Data_%s_bleaching.h5', DateString);
+    else
+        FileName = sprintf('Data_%s.h5', DateString);
     end
+    FileName = fullfile(DirectoryName, FileName);
 
-    % FF test for save h.file
+    % Prepare for .h5 file writing.
+    % NOTE: For now, obj.SaveFileType must be 'h5'.
     switch obj.SaveFileType
-        case 'mat'
         case 'h5'
-            FileH5=FileName; %FF
+            FileH5 = FileName;
             MIC_H5.createFile(FileH5);
-            MIC_H5.createGroup(FileH5,'Channel01');
-            MIC_H5.createGroup(FileH5,'Channel01/Zposition001');
+            MIC_H5.createGroup(FileH5, 'Channel01');
+            MIC_H5.createGroup(FileH5, 'Channel01/Zposition001');
         otherwise
             error('StartSequence:: unknown file save type')
     end
 
-    %Move to Cell
-    obj.StageStepper.moveToPosition(1,RefStruct.StepperPos(2)+obj.CoverSlipOffset(2)); %new %y
-    obj.StageStepper.moveToPosition(2,RefStruct.StepperPos(1)+obj.CoverSlipOffset(1)); %new %x
-    obj.StageStepper.moveToPosition(3,RefStruct.StepperPos(3)+obj.CoverSlipOffset(3)); %new %z
-    obj.StagePiezoX.center(); %new 
-    obj.StagePiezoY.center(); %new
-    obj.StagePiezoZ.center(); %new 
-    %Align
-    obj.Lamp660.setPower(obj.Lamp660Power+2);
+    % Attempt to move to the cell of interest.
+    obj.StageStepper.moveToPosition(1, ...
+        RefStruct.StepperPos(2) + obj.CoverSlipOffset(2));
+    obj.StageStepper.moveToPosition(2, ...
+        RefStruct.StepperPos(1) + obj.CoverSlipOffset(1));
+    obj.StageStepper.moveToPosition(3, ...
+        RefStruct.StepperPos(3) + obj.CoverSlipOffset(3));
+    obj.StagePiezoX.center();
+    obj.StagePiezoY.center();
+    obj.StagePiezoZ.center();
+    
+    % Attempt to align the cell to the reference image in brightfield.
+    obj.Lamp660.setPower(obj.Lamp660Power);
     pause(obj.LampWait);
-    obj.CameraSCMOS.ExpTime_Capture=obj.ExposureTimeCapture; %need to update when changing edit box
+    obj.CameraSCMOS.ExpTime_Capture = obj.ExposureTimeCapture; 
     obj.CameraSCMOS.AcquisitionType = 'capture';
-    obj.CameraSCMOS.ROI=obj.SCMOS_ROI_Collect;
+    obj.CameraSCMOS.ROI = obj.SCMOS_ROI_Collect;
     obj.CameraSCMOS.setup_acquisition();
     obj.AlignReg.Image_Reference=RefStruct.Image;
-    try %So that if alignment fails, we don't stop auto collect for other cells
-        obj.AlignReg.align2imageFit(RefStruct); %FF
-    catch 
+    try 
+        obj.AlignReg.align2imageFit(RefStruct);
+    catch
+        % We don't want to throw an error since there are still other cells
+        % to be measured from here on.
         warning('Problem with AlignReg.align2imageFit()')
         return
     end
-
     obj.Lamp660.setPower(0);
 
     % Setup Active Stabilization (if desired).
@@ -56,20 +67,21 @@
         obj.Lamp850.on; 
         obj.Lamp850.setPower(obj.Lamp850Power);
         obj.IRCamera_ExposureTime=obj.CameraIR.ExpTime_Capture;
-        obj.ActiveReg.takeRefImageStack(); %takes 21 reference images
+        obj.ActiveReg.takeRefImageStack(); % takes 21 reference images
         obj.ActiveReg.Period=obj.StabPeriod;
         obj.ActiveReg.start();
     end
 
-    %Setup sCMOS for Sequence
-    obj.CameraSCMOS.ExpTime_Sequence=obj.ExposureTimeSequence;
-    obj.CameraSCMOS.SequenceLength=obj.NumberOfFrames;
-    obj.CameraSCMOS.ROI=obj.SCMOS_ROI_Collect;
+    % Setup the main sCMOS to acquire to sequence.
+    obj.CameraSCMOS.ExpTime_Sequence = obj.ExposureTimeSequence;
+    obj.CameraSCMOS.SequenceLength = obj.NumberOfFrames;
+    obj.CameraSCMOS.ROI = obj.SCMOS_ROI_Collect;
     obj.CameraSCMOS.AcquisitionType = 'sequence';
     obj.CameraSCMOS.setup_acquisition();
 
-    %Start Laser
-    obj.FlipMount.FilterOut; % moves away the ND filter from the beam
+    % Send the 647 nm laser to the sample.  If requested, also send the 
+    % 405 nm laser to the sample.
+    obj.FlipMount.FilterOut(); % removes ND filter from optical path
     if obj.Use405
         obj.Laser405.setPower(obj.LaserPower405Activate);
     end
@@ -77,101 +89,82 @@
         obj.Laser405.setPower(obj.LaserPower405Bleach);
     end
 
-    %Collect Data
-    if obj.IsBleach
-        for nn=1:obj.NumberOfPhotoBleachingIterations
-            obj.Shutter.open;
-            if obj.Use405
-                obj.Laser405.on();
-            end
-            Data=obj.CameraSCMOS.start_sequence();
-            S=sprintf('F.Data%2.2d=Data;',nn);
-            eval(S);
-            obj.Shutter.close; % closes shutter before the Laser turns on
-        end
-    else
-        fprintf('Collecting data...................................... \n')
-        for nn=1:obj.NumberOfSequences
-            % Use periodic registration after NSeqBeforePeriodicReg
-            % sequences have been collected.
-            if obj.UsePeriodicReg && ~mod(nn, obj.NSeqBeforePeriodicReg)
-                obj.Lamp660.setPower(obj.Lamp660Power);
-                pause(obj.LampWait);
-                obj.CameraSCMOS.ExpTime_Capture=obj.ExposureTimeCapture; 
-                obj.CameraSCMOS.AcquisitionType = 'capture';
-                obj.CameraSCMOS.ROI=obj.SCMOS_ROI_Collect;
-                obj.CameraSCMOS.setup_acquisition();
-                obj.AlignReg.Image_Reference=RefStruct.Image;
-                try
-                    obj.AlignReg.align2imageFit(RefStruct);
-                catch 
-                    % If the alignment fails, don't stop auto collect for 
-                    % other cells.
-                    warning('Problem with AlignReg.align2imageFit()')
-                    return
-                end
-                obj.Lamp660.setPower(0);
-            end
-            
-            if obj.Use405
-                obj.Laser405.on();
-            end
-            obj.Shutter.open; % opens shutter before the Laser turns on
-
-            %Collect
-            obj.CameraSCMOS.AcquisitionType = 'sequence';
-            obj.CameraSCMOS.ExpTime_Sequence=obj.ExposureTimeSequence;
+    % Begin the acquisition.
+    fprintf('Collecting data.......................................... \n')
+    for ii = 1:obj.NumberOfSequences
+        % Use periodic registration after NSeqBeforePeriodicReg
+        % sequences have been collected.
+        if obj.UsePeriodicReg && ~mod(ii, obj.NSeqBeforePeriodicReg)
+            obj.Lamp660.setPower(obj.Lamp660Power);
+            pause(obj.LampWait);
+            obj.CameraSCMOS.ExpTime_Capture = obj.ExposureTimeCapture;
+            obj.CameraSCMOS.AcquisitionType = 'capture';
+            obj.CameraSCMOS.ROI = obj.SCMOS_ROI_Collect;
             obj.CameraSCMOS.setup_acquisition();
-            sequence=obj.CameraSCMOS.start_sequence();
-            if ~obj.IsBleach %Append Data
-                obj.Shutter.close;
-                switch obj.SaveFileType
-                    case 'mat'
-                        fn=fullfile(obj.SaveDir,[obj.BaseFileName '#' num2str(nn,'%04d') s]);
-                        Params=exportState(obj);
-                        save(fn,'sequence','Params');
-                    case 'h5' %This will become default
-                        S=sprintf('Data%04d',nn);
-                        MIC_H5.writeAsync_uint16(FileH5,'Channel01/Zposition001',S,sequence);
-                    otherwise
-                        error('StartSequence:: unknown SaveFileType')
-                end
-                %obj.Shutter.open;
+            obj.AlignReg.Image_Reference = RefStruct.Image;
+            try
+                obj.AlignReg.align2imageFit(RefStruct);
+            catch
+                % If the alignment fails, don't stop auto collect for
+                % other cells.
+                warning('Problem with AlignReg.align2imageFit()')
+                return
             end
-            obj.Shutter.close;
-            if obj.Use405
-                obj.Laser405.off();
-            end
+            obj.Lamp660.setPower(0);
         end
-        fprintf('Data collection complete \n')
         
-        %End Laser
-        obj.Shutter.close; % closes the shutter instead of turning off the Laser
-        obj.FlipMount.FilterIn; %new
+        % Turn on the 405 nm laser (if needed) and open the shutter to
+        % allow the 647 nm laser to reach the sample.
         if obj.Use405
-            obj.Laser405.setPower(0);
+            obj.Laser405.on();
+        end
+        obj.Shutter.open();
+        
+        % Collect the sequence.
+        obj.CameraSCMOS.AcquisitionType = 'sequence';
+        obj.CameraSCMOS.ExpTime_Sequence = obj.ExposureTimeSequence;
+        obj.CameraSCMOS.setup_acquisition();
+        Sequence = obj.CameraSCMOS.start_sequence();
+        obj.Shutter.close();
+        switch obj.SaveFileType
+            % For now, we will stick to saving data to a .h5 file.
+            case 'h5'
+                SequenceName = sprintf('Data%04d', ii);
+                MIC_H5.writeAsync_uint16(FileH5,'Channel01/Zposition001',SequenceName,Sequence);
+            otherwise
+                error('StartSequence:: unknown SaveFileType')
+        end
+        obj.Shutter.close(); % block 647 nm from reaching sample
+        if obj.Use405
+            % Turn off the 405 nm laser.
             obj.Laser405.off();
         end
-
-        %End Active Stabilization:
-        if obj.UseActiveReg
-            obj.ActiveReg.stop();
-        end
-
-        %Save Everything
-        if ~obj.IsBleach %Append Data
-            fprintf('Saving exportables from exportState()............ \n')
-            switch obj.SaveFileType
-                case 'mat'
-                    %Nothing to do
-                case 'h5'
-                    S='Channel01/Zposition001';
-                    MIC_H5.createGroup(FileH5,S);
-                    obj.save2hdf5(FileH5,S);
-                otherwise
-                    error('StartSequence:: unknown SaveFileType')
-            end
-            fprintf('Saving exportables from exportState() complete \n')
-        end
     end
+    fprintf('Data collection complete \n')
+    
+    % Ensure that the lasers are no reaching the sample.
+    obj.Shutter.close(); % close shutter instead of turning off the laser
+    obj.FlipMount.FilterIn();
+    if obj.Use405
+        obj.Laser405.setPower(0);
+        obj.Laser405.off();
+    end
+    
+    % If it was used, end the active stabilization process.
+    if obj.UseActiveReg
+        obj.ActiveReg.stop();
+    end
+    
+    % Save the acquisition data.
+    fprintf('Saving exportables from exportState().................... \n')
+    switch obj.SaveFileType
+        % For now, we will stick to saving data to a .h5 file.
+        case 'h5'
+            SequenceName = 'Channel01/Zposition001';
+            MIC_H5.createGroup(FileH5, SequenceName);
+            obj.save2hdf5(FileH5, SequenceName);
+        otherwise
+            error('StartSequence:: unknown SaveFileType')
+    end
+    fprintf('Saving exportables from exportState() complete \n')
  end
