d02174c1e219f3abd856964fb8161aaebf32ce64
sajjad88 <56313813+sajjad88@users.noreply.github.com>
2021-08-25 11:54:03 -0700

    Create MIC_MCLObjPiezo.m
    Mad City Labs objective piezo controller class.

---
+++ @MIC_MCLObjPiezo/MIC_MCLObjPiezo.m
@@ -0,0 +1,21 @@
+classdef MIC_MCLObjPiezo < MIC_Abstract
+    % This class is inheriting from MIC_Abstract. 
+    % This class is written to control the Mad City Labs objective
+    % piezo controller.
+    
+    % REQUIREMENTS: 
+    %   MIC_Abstract.m
+    %   MATLAB software version R2016b or later
+    
+    % CITATION: Sajjad Khan, LidkeLab, 2021.
+    
+    
+    properties (SetAccess=protected)
+        InstrumentName
+    end
+    
+    methods
+        % Some method will go here
+    end 
+    
+end
