c0f4f492ff251cd9acd670224ef2bc97c8243c38
dschodt <35635770+dschodt@users.noreply.github.com>
2021-09-01 16:47:38 -0600

    Fix a bug in MIC_Triggerscope.gui()

--- @MIC_Triggerscope/gui.m
+++ @MIC_Triggerscope/gui.m
@@ -250,12 +250,13 @@ propertiesToGUI();
         end
     end
 
-    function guiToProperties()
+    function guiToProperties(~, ~)
         % This function updates properties in obj based on changes made to
         % controls in this GUI.
         
         % Update the serial port popup menu.
-        obj.SerialPort = ControlHandles.SerialPortPopup.String;
+        obj.SerialPort = ControlHandles.SerialPortPopup.String(...
+            ControlHandles.SerialPortPopup.Value);
         
         % Update the status of the TTL channels.
         for ii = 1:numel(obj.TTLStatus)
