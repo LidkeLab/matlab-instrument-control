2de20ca1d157fbca8480e383776f4ceb3f466367
kiwibogo <kiwibogo@gmail.com>
2024-05-06 10:28:02 -0600

    delete SEQ collect

--- @MIC_SEQ_SRcollect/private/IRCaptureTimerFcnV1.m
+++
@@ -1,9 +0,0 @@
-function IRCaptureTimerFcnV1(obj,event,ActRegObj,SaveDir,FileName)
-ActRegObj.align2imageFit;
-Params=struct(ActRegObj);
-Params.CameraObj=[];
-Params.StageObj=[];
-event_time=datestr(event.Data.time,'mm-dd-yy-HH-MM-SS');
-fn=fullfile(SaveDir,[FileName,event_time]);
-save(fn,'Params');
-end
