e1d713e73a51c870b150fb16fa6175d6cd7305ed
sajjad88 <56313813+sajjad88@users.noreply.github.com>
2024-11-26 15:09:22 -0700

    Update startup.m

--- startup.m
+++ startup.m
@@ -1,4 +1,5 @@
 % Paths tp be added to startup.m for matlab-instrument-control.
 addpath '~/Documents/MATLAB/matlab-instrument-control/src'
 addpath '~/Documents/MATLAB/matlab-instrument-control/mex64'
+addpath '~/Documents/MATLAB/matlab-instrument-control/mex64/dcam4'
 
